a37026258e13061f9e4ea8aa7201d453eae92b86
hwvs <hwvs@github.com>
2021-05-21 15:30:49 -0700

    init

---
+++ SarahPeterkov/Program.cs
@@ -0,0 +1,24 @@
+﻿using MarkovSharp.TokenisationStrategies;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace SarahPeterkov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("corpus.txt");
+
+            var model = new StringMarkov(2);
+            model.Learn(lines);
+
+            while (true)
+            {
+                Console.WriteLine(model.Walk().First());
+                Console.ReadLine();
+            }
+        }
+    }
+}
